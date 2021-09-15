e7e38fb05992b35f2019d811a6328dc314cd5100
Stuart B. Wilkins <swilkins@bnl.gov>
2021-09-15 05:06:05 -0400

    Patched for build

--- src/engine.c
+++ src/engine.c
@@ -31,7 +31,10 @@
 void 
 queryIdleTime (Display* d, Bool use_xidle)
 {
-  Time idleTime = 0; /* millisecs since last input event */
+  int dummy;
+  CARD16 standby, suspend, off;
+  CARD16 state;
+  BOOL onoff;  Time idleTime = 0; /* millisecs since last input event */
 
 #ifdef HasXidle
   if (use_xidle)
@@ -47,6 +50,33 @@ queryIdleTime (Display* d, Bool use_xidle)
     XScreenSaverQueryInfo (d, DefaultRootWindow (d), mitInfo);
     idleTime = mitInfo->idle;
 #endif /* HasScreenSaver */
+  }
+
+  if (DPMSQueryExtension(d, &dummy, &dummy)) {
+    if (DPMSCapable(d)) {
+      DPMSGetTimeouts(d, &standby, &suspend, &off);
+      DPMSInfo(d, &state, &onoff);
+
+      if (onoff) {
+        switch (state) {
+          case DPMSModeStandby:
+            if (idleTime < (unsigned) (standby * 1000))
+              idleTime += (standby * 1000);
+            break;
+          case DPMSModeSuspend:
+            if (idleTime < (unsigned) ((suspend + standby) * 1000))
+              idleTime += ((suspend + standby) * 1000);
+            break;
+          case DPMSModeOff:
+            if (idleTime < (unsigned) ((off + suspend + standby) * 1000))
+              idleTime += ((off + suspend + standby) * 1000);
+            break;
+          case DPMSModeOn:
+          default:
+            break;
+        }
+      }
+    } 
   }
 
   if (idleTime < 1000)  
@@ -210,7 +240,7 @@ evaluateTriggers (Display* d)
 #else /* VMS */
   if (lockerPid)
   {
-#if !defined (UTEKV) && !defined (SYSV) && !defined (SVR4)
+#if !defined (UTEKV) && !defined (SYSV) && !defined (SVR4) && !defined (__GLIBC__)
     union wait  status;      /* childs process status */
 #else /* !UTEKV && !SYSV && !SVR4 */
     int         status = 0;  /* childs process status */
@@ -221,7 +251,7 @@ evaluateTriggers (Display* d)
       (void) kill (lockerPid, SIGTERM);
     }
 
-#if !defined (UTEKV) && !defined (SYSV) && !defined (SVR4)
+#if !defined (UTEKV) && !defined (SYSV) && !defined (SVR4) && !defined (__GLIBC__)
     if (wait3 (&status, WNOHANG, 0))
 #else /* !UTEKV && !SYSV && !SVR4 */
     if (waitpid (-1, &status, WNOHANG)) 
